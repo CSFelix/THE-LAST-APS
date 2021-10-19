9e50a306b537243991d548e59f2001bec88259ed
CSFelix <57205604+CSFelix@users.noreply.github.com>
2021-10-18 21:10:38 -0300

    Update ajax-visualizar-post.js

--- WebContent/assets/js/ajax/ajax-visualizar-post.js
+++ WebContent/assets/js/ajax/ajax-visualizar-post.js
@@ -4,18 +4,12 @@ const inputEnderecoVisualizarPost = document.getElementById("inputEndereco");
 const inputDataCriacaoVisualizarPost = document.getElementById("inputDataCriacao");
 const inputStatusSelecionadoVisualizarPost = document.getElementById("statusSelecionado");
 const inputComentarioVisualizarPost = document.getElementById("inputComentario");
-
 var params;
 var req;
 
-// informação deve estar previamente armazenada na session storage do navegador
-// para tal, o valor deve-ser armazenado a partir do momento em que o usuário
-// clica para ver a postagem
-params = "id=" + sessionStorage.getItem("idPost"); 
-
 // AJAX
 req = new XMLHttpRequest();
-req.open("POST", "http://localhost:7777/", true);
+req.open("GET", "http://localhost:3000//autoridade/" + localStorage.getItem("idPost"), true);
 req.onreadystatechange = function() {
 	
 	// Servidor Fora do Ar
@@ -37,4 +31,4 @@ req.onreadystatechange = function() {
 // Definição do header do método HTTP POST
 // e envio dos parãmetros
 req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-req.send(params);
+req.send();
