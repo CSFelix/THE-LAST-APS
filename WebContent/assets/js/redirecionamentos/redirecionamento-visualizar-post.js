41c3f1cdaebf6470629999a8a13d59b00749b5e2
CSFelix <57205604+CSFelix@users.noreply.github.com>
2021-11-03 20:49:07 -0300

    Delete redirecionamento-visualizar-post.js

--- WebContent/assets/js/redirecionamentos/redirecionamento-visualizar-post.js
+++
@@ -1,6 +0,0 @@
-function RedirecionarVisualizarPost(botaoVisualizarPost) {
-	window.location.href = "visualizar-post.jsp?idPost="
-						  + botaoVisualizarPost.dataset.id
-						  + "&idioma="
-						  + localStorage.getItem('language'); 
-}
