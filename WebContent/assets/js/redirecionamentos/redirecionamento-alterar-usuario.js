0bdb9704c3393dc5d60b543ce963410ad8d315e2
CSFelix <57205604+CSFelix@users.noreply.github.com>
2021-11-03 20:49:02 -0300

    Delete redirecionamento-alterar-usuario.js

--- WebContent/assets/js/redirecionamentos/redirecionamento-alterar-usuario.js
+++
@@ -1,6 +0,0 @@
-function RedirecionarAlterarUsuario(botaoAlterarUsuario) {
-	window.location.href = "alterar-usuario.jsp?idUsuario="
-						  + botaoAlterarUsuario.dataset.id
-						  + "&idioma="
-						  + localStorage.getItem('language'); 
-}
