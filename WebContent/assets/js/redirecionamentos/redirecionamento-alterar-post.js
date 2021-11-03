d5bf5b7f93e5ca87af7397d5e600c606f2f55af3
CSFelix <57205604+CSFelix@users.noreply.github.com>
2021-11-03 20:48:55 -0300

    Delete redirecionamento-alterar-post.js

--- WebContent/assets/js/redirecionamentos/redirecionamento-alterar-post.js
+++
@@ -1,6 +0,0 @@
-function RedirecionarAlterarPost(botaoAlterarPost) {
-	window.location.href = "alterar-post.jsp?idPost="
-						  + botaoAlterarPost.dataset.id
-						  + "&idioma="
-						  + localStorage.getItem('language'); 
-}
