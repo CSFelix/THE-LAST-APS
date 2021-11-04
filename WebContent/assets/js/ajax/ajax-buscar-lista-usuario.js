6df4db6a83bb8262cc7bd1f714bf52c2ee8dc975
CSFelix <57205604+CSFelix@users.noreply.github.com>
2021-11-03 22:10:12 -0300

    🎉 Updates

--- WebContent/assets/js/ajax/ajax-buscar-lista-usuario.js
+++ WebContent/assets/js/ajax/ajax-buscar-lista-usuario.js
@@ -1,57 +1,51 @@
-const botaoAtualizarTabelaListaUsuariosFiltroNome = document.getElementById("botaoAtualizarTabelaListaUsuarios");
-const tabelaListaUsuariosFiltroNome = document.getElementById("tabelaListaUsuarios");
+const botaoAtualizarTabelaListaUsuarios = document.getElementById("botaoAtualizarTabelaListaUsuarios");
+const tabelaListaUsuarios = document.getElementById("tabelaListaUsuarios");
 
-const inputNomePesquisaFiltroNome = document.getElementById("inputNomePesquisa");
-const toastMessageUsuariosFiltroNome = document.getElementById("toast");
+const toastMessageUsuarios = document.getElementById("toast");
 
 var mensagemErro;
-var params;
 var req;
 
-// clique no botão de busca de usuários com filtro no nome
-botaoAtualizarTabelaListaUsuariosFiltroNome.addEventListener("click", () => { BuscarDadosUsuariosComFiltro(); });
+// clique no botão de busca de usuários
+botaoAtualizarTabelaListaUsuarios.addEventListener("click", () => { BuscarDadosUsuariosSemFiltro(); });
 
 // função para busca dos dados
-function BuscarDadosUsuariosComFiltro() {
+function BuscarDadosUsuariosSemFiltro() {
 	
-	// busca é feita somente se input de nome está preenchido
-	if (inputNomePesquisaFiltroNome.value != "") {
+	// AJAX
+	req = new XMLHttpRequest();
+	req.open("GET", "http://localhost:3000/autoridade/all", true);
+	req.onreadystatechange = function() {
 		
-		// AJAX
-		req = new XMLHttpRequest();
-		req.open("GET", "http://localhost:3000/autoridade/find-nome/" + inputNomePesquisaFiltroNome.value, true);
-		req.onreadystatechange = function() {
+		//	Servidor Fora do Ar
+		if (req.readyState != 4 || req.status != 200) {  }
 			
-			//	Servidor Fora do Ar
-			if (JSON.parse(this.responseText).status == 500) {  }
+			// Sucesso ou Falha na Busca por Posts
+			else {
 				
-				// Sucesso ou Falha na Busca por Posts
-				else {
+				// Falha Interna
+				if (JSON.parse(this.responseText).status == 500) { ExibirToastMessage(3); }
+				
+				// Alteração Efetuada com sucesso
+				else { 
+					var dados = JSON.parse(this.responseText);
 					
-					// Falha Interna
-					if (JSON.parse(this.responseText).status == 500) { ExibirToastMessage(3); }
-					
-					// Alteração Efetuada com sucesso
-					else { 
-						var dados = JSON.parse(this.responseText);
-						
-						for (i = 0; i < dados.length; i++) {
-							tabelaListaUsuarios.innerHTML += "<tr>"
-								   						   + "	<button onclick='RedirecionarAlterarUsuario(this)' class='botao' data-id='" + dados[i].id + "' data-anijs='if: mouseover, do: rubberBand animated'>Update / Alterar</button>"
-								   						   + "	<td>" + dados[i].nome + "</td>"
-								   						   + "	<td>" + dados[i].login + "</td>"
-								   						   + "	<td>" + dados[i].grauAutoridade + "</td>"
-								   						   + "	<td>" + dados[i].dataCriada + "</td>"
-								   						   + "	<td>" + dados[i].ativo + "</td>"
-								   						   + "</tr>";
-						}
+					for (i = 0; i < dados.length; i++) {
+						tabelaListaUsuarios.innerHTML += "<tr>"
+							+ "	<td><button onclick='RedirecionarAlterarUsuario(this)' class='botao' data-id='" + dados[i].id + "' data-anijs='if: mouseover, do: rubberBand animated'>Update / Alterar</button></td>"
+							   						   + "	<td>" + dados[i].nome + "</td>"
+							   						   + "	<td>" + dados[i].login + "</td>"
+							   						   + "	<td>" + dados[i].grauAutoridade + "</td>"
+							   						   + "	<td>" + dados[i].dataCriada + "</td>"
+							   						   + "	<td>" + dados[i].ativo + "</td>"
+							   						   + "</tr>";
 					}
 				}
-			};
-			
-		// Definição do header do método HTTP POST
-		// e envio dos parãmetros
-		req.setRequestHeader('Authorization', localStorage.getItem("Authorization"));
-		req.send();
-	}
+			}
+		};
+		
+	// Definição do header do método HTTP POST
+	// e envio dos parãmetros
+	req.setRequestHeader('Authorization', localStorage.getItem("Authorization"));
+	req.send();
 }
